42f44163da7494f00951369dd61a072c667a1580
light7662 <alexbadminlol@hotmail.com>
2019-09-11 13:24:48 -0400

    cours 4

---
+++ Cours 4/Cours 4/Assets/rotation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    Vector3 newRotation = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newRotation = transform.rotation.eulerAngles;
+        if (Input.GetKey(KeyCode.A))
+        {
+            newRotation.z = newRotation.z + 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            newRotation.z = newRotation.z - 1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            newRotation.y = newRotation.y + 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            newRotation.y = newRotation.y - 1f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newRotation.x = newRotation.x + 1f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newRotation.x = newRotation.x - 1f;
+        }
+        transform.rotation = Quaternion.Euler(newRotation);
+        /*newRotation = transform.rotation.eulerAngles;
+        newRotation.y = newRotation.y + 1f;
+        //newRotation.x = newRotation.x + 1f;
+        newRotation.z = newRotation.z + 1f;
+        transform.rotation = Quaternion.Euler(newRotation);*/
+    }
+}
---
+++ Cours 4/Cours 4/Assets/thirdMouvement.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdMouvement : MonoBehaviour
+{
+    public Rigidbody rb;
+    //public float speed;
+    public float turnSpeed = 10f;
+    //public float JumpHeight = 0;
+    //Vector3 newPos;
+    Vector3 velocity;
+    public float jumpSpeed = 200f;
+    private bool m_canJump;
+    public float x_speed;
+    public float z_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        //newPos = new Vector3();
+        m_canJump = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*
+        //newPos = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            //newPos.z = newPos.z + speed;
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            //newPos.z = newPos.z - speed;
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //newPos.x = newPos.x - speed;
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //newPos.x = newPos.x + speed;
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        //transform.position = newPos;*/
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+
+        velocity = transform.InverseTransformDirection(rb.velocity);
+        //velocity = rb.velocity;
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.z = z_speed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            velocity.z = -z_speed;
+        }
+        else
+        {
+            velocity.z = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = x_speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -x_speed;
+        }
+        else
+        {
+            velocity.x = 0f;
+        }
+
+        if (m_canJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, jumpSpeed, 0);// addForce donne un coup vers une position
+            m_canJump = false;
+        }
+        rb.velocity = transform.TransformDirection(velocity);
+    }
+    private void FixedUpdate()
+    {
+        //rb.velocity = velocity; //velocity m/s
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_canJump = true;
+    }
+}
