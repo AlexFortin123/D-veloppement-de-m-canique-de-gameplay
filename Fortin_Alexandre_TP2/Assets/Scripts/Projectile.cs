4261e1c64426a898a203785ad9be0f6e5cbd30dc
light7662 <alexbadminlol@hotmail.com>
2019-10-17 19:45:39 -0400

    Avancement TP2

---
+++ Fortin_Alexandre_TP2/Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float m_Speed;
+    public GameObject m_Tireur;
+    private Vector3 m_Pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Pos = transform.position;
+        m_Pos.z += m_Speed;
+        transform.position = m_Pos;
+        
+        if(Vector3.Distance(m_Tireur.transform.position, transform.position) > 50f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
