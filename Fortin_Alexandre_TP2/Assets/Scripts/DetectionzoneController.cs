73872ff407e89e9c65915af7550e1abd27e2b805
light7662 <alexbadminlol@hotmail.com>
2019-10-30 19:25:36 -0400

    Cours 10

---
+++ Fortin_Alexandre_TP2/Assets/Scripts/DetectionzoneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionzoneController : MonoBehaviour
+{
+    public bool m_ObjectDetected;
+    public Transform m_TransformTarget;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_ObjectDetected = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            m_ObjectDetected = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_ObjectDetected = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            m_TransformTarget = other.gameObject.transform;
+        }
+    }
+}
--- Fortin_Alexandre_TP2/Assets/Scripts/EnnemisController.cs
+++ Fortin_Alexandre_TP2/Assets/Scripts/EnnemisController.cs
@@ -6,33 +6,61 @@ public class EnnemisController : MonoBehaviour
 {
     public int m_Hp;
     public List<GameObject> m_NoeudPartrouille;
-    private Vector3 m_StartingPos;
+    public GameObject m_DetectZonePlayer;
     public float m_Speed;
+    public GameObject m_balleEnnemy;
+    public int m_WaitingTimeShoot;
+
+    private Vector3 m_StartingPos;
     private int index;
     private float m_Pourcentage;
-    
+    private GameObject m_zone;
+    private DetectionzoneController m_DetecteZone;
+    private float timer;
+
     private void Awake()
     {
         index = 1;
         m_Pourcentage = 0f;
         m_StartingPos = transform.position;
+        m_zone = GameObject.Instantiate(m_DetectZonePlayer, gameObject.transform.position, Quaternion.identity);
+        m_zone.transform.SetParent(this.transform);
+        m_DetecteZone = m_zone.GetComponent<DetectionzoneController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(m_StartingPos, m_NoeudPartrouille[index].transform.position, m_Pourcentage);
-        if(m_Pourcentage >= 1)
+        if (!m_DetecteZone.m_ObjectDetected)
         {
-            index++;
-            m_Pourcentage = 0;
-            m_StartingPos = transform.position;
+            transform.position = Vector3.Lerp(m_StartingPos, m_NoeudPartrouille[index].transform.position, m_Pourcentage);
+            if (m_Pourcentage >= 1)
+            {
+                index++;
+                m_Pourcentage = 0;
+                m_StartingPos = transform.position;
+            }
+            if (index >= m_NoeudPartrouille.Count)
+            {
+                index = 0;
+            }
+            m_Pourcentage += m_Speed * Time.deltaTime; //Permet de rendre constant le pourcentage
         }
-        if(index >= m_NoeudPartrouille.Count)
+        else
         {
-            index = 0;
+            Debug.Log(m_DetecteZone.transform.position);
+            timer += Time.deltaTime;
+            if(timer > m_WaitingTimeShoot)
+            {
+                GameObject projectileGameObject = GameObject.Instantiate(m_balleEnnemy, transform.position + 2f * Vector3.forward, Quaternion.identity);
+                projectileGameObject.transform.rotation = transform.rotation;
+                projectileGameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 1000f);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(m_DetecteZone.transform.position - transform.position),
+                    3f * Time.deltaTime);
+                timer = 0;
+            }
+            transform.position += transform.forward * Time.deltaTime;
         }
-        m_Pourcentage += m_Speed * Time.deltaTime;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -45,4 +73,5 @@ public class EnnemisController : MonoBehaviour
             }
         }
     }
+   
 }
