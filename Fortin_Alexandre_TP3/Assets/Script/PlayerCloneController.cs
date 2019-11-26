9c07629bd2751cb2507de4ddb14c13cf01a6b328
light7662 <alexbadminlol@hotmail.com>
2019-11-26 18:01:54 -0500

    Avancement TP3

---
+++ Fortin_Alexandre_TP3/Assets/Script/PlayerCloneController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCloneController : MonoBehaviour
+{
+    public GameObject m_Player;
+    public Rigidbody m_ClonePlayerRigidbody;
+
+    private Vector3 m_VelocityClonePlayer;
+    private PlayerController m_PlayerController;
+    private float m_SpeedMouvementClonePlayer;
+    private float m_JumpForceClonePlayer;
+
+    private void Awake()
+    {
+        //m_PlayerController = m_Player.GetComponent<PlayerController>();
+        m_ClonePlayerRigidbody = GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        m_PlayerController = m_Player.GetComponent<PlayerController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = m_Player.transform.rotation;
+        m_SpeedMouvementClonePlayer = m_PlayerController.m_SpeedMouvementPlayer;
+        m_JumpForceClonePlayer = m_PlayerController.m_JumpForcePlayer;
+
+        m_VelocityClonePlayer = m_ClonePlayerRigidbody.velocity;
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_VelocityClonePlayer = transform.forward * m_SpeedMouvementClonePlayer;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            m_VelocityClonePlayer = transform.forward * -m_SpeedMouvementClonePlayer;
+        }
+        else
+        {
+            m_VelocityClonePlayer = Vector3.zero;
+        }
+        m_ClonePlayerRigidbody.velocity = m_VelocityClonePlayer;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_ClonePlayerRigidbody.AddForce(0, m_JumpForceClonePlayer, 0);
+        }
+    }
+}
--- Fortin_Alexandre_TP3/Assets/Script/PlayerController.cs
+++ Fortin_Alexandre_TP3/Assets/Script/PlayerController.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Script qui définie le comportement du player
 public class PlayerController : MonoBehaviour
 {
     public float m_SpeedMouvementPlayer;
     public float m_JumpForcePlayer;
+    public Vector3 m_NewScaleShrinkPlayer;
     public Rigidbody m_RigidbodyPlayer;
+    public GameObject m_ClonePlayer;
 
     private Vector3 m_VelocityPlayer;
+    private Vector3 m_ScalePlayerDefault;
+    private float m_SpeedMouvementPlayerDefault;
+    private float m_JumpForcePlayerDefault;
 
     private void Awake()
     {
-        m_RigidbodyPlayer = gameObject.GetComponent<Rigidbody>();
+        m_RigidbodyPlayer = GetComponent<Rigidbody>();
+        m_SpeedMouvementPlayerDefault = m_SpeedMouvementPlayer;
+        m_JumpForcePlayerDefault = m_JumpForcePlayer;
+        m_ScalePlayerDefault = transform.localScale;
     }
     // Update is called once per frame
     void Update()
@@ -34,7 +43,43 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_RigidbodyPlayer.AddForce(Vector3.up * m_JumpForcePlayer);
+            m_RigidbodyPlayer.AddForce(0 , m_JumpForcePlayer, 0);
         }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ResetButton")
+        {
+            ResetPlayerStat();
+        }
+        if(other.gameObject.tag == "CloneButton")
+        {
+            CreateClonePlayer();
+        }
+        if(other.gameObject.tag == "ShrinkButton")
+        {
+            Shrink();
+        }
+    }
+    public void SetJumpForce()
+    {
+        m_JumpForcePlayer *= 2f;
+    }
+    public void ResetPlayerStat()
+    {
+        m_SpeedMouvementPlayer = m_SpeedMouvementPlayerDefault;
+        m_JumpForcePlayer = m_JumpForcePlayerDefault;
+        transform.localScale = m_ScalePlayerDefault;
+    }
+    public void CreateClonePlayer()
+    {
+        GameObject ClonePlayer = GameObject.Instantiate(m_ClonePlayer, transform.position + transform.forward * 2f, Quaternion.identity);
+        ClonePlayer.GetComponent<PlayerCloneController>().m_Player = gameObject;
+        //ClonePlayer.GetComponent<PlayerController>().enabled = false;
+    }
+    public void Shrink()
+    {
+        transform.localScale = m_NewScaleShrinkPlayer;
     }
 }
